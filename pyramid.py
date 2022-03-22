1ebfb40c7154469ecea7e235c43913019dcada0c
ishika <78346642+ishiikaa@users.noreply.github.com>
2022-03-22 20:40:34 +0530

    done

---
+++ pyramid.py
@@ -0,0 +1,6 @@
+for i in range(4,-1):
+    print("*")
+    i = i+1
+    for j in range(1,i):
+        print("*")
+        j = j+1
---
+++ reverse_multab.py
@@ -0,0 +1,7 @@
+veer = int(input("Enter your pp size to be multiplied: "))
+dick = 10
+pussy = 10
+for pussy in reversed(range(1, 11)):
+    print(veer, "*", dick, "=", veer * pussy)
+    dick = dick - 1
+    pussy = pussy + 1
